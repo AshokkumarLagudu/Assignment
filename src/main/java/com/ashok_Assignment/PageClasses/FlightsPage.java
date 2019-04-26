bcb2e2a835203e957d93834fac3e4502a9a7600c
AshokkumarLagudu <ashokkumalagudu@gmail.com>
2019-04-26 22:51:59 +0530

    Update by ashok

--- src/main/java/com/ashok_Assignment/PageClasses/FlightsPage.java
+++ src/main/java/com/ashok_Assignment/PageClasses/FlightsPage.java
@@ -13,220 +13,217 @@ import com.ashok_Assignment.Base.BaseClass;
 import com.ashok_Assignment.utils.UtilClass;
 
 public class FlightsPage extends BaseClass {
-	
+
 	WebDriverWait wait = new WebDriverWait(driver, UtilClass.wedriverWait);
-	
-	@FindBy(xpath="//div[@class='splitVw-sctn pull-left']//div[2]//div[@class='fli-list splitVw-listing']")
+
+	@FindBy(xpath = "//div[@class='splitVw-sctn pull-left']//div[2]//div[@class='fli-list splitVw-listing']")
 	private List<WebElement> departureFlights;
-	
-	@FindBy(xpath="//div[@class='splitVw-sctn pull-right']//div[2]//div[@class='fli-list splitVw-listing']")
+
+	@FindBy(xpath = "//div[@class='splitVw-sctn pull-right']//div[2]//div[@class='fli-list splitVw-listing']")
 	private List<WebElement> returnFlights;
-	
-	@FindBy(xpath="//span[@class='labeltext' and text()='Non Stop']//parent::span//parent::label[@for='filter_stop0']/span/span")
+
+	@FindBy(xpath = "//span[@class='labeltext' and text()='Non Stop']//parent::span//parent::label[@for='filter_stop0']/span/span")
 	private WebElement nonStop;
-	
-	@FindBy(xpath="//span[@class='labeltext' and text()='1 Stop']//parent::span//parent::label[@for='filter_stop1']/span/span")
+
+	@FindBy(xpath = "//span[@class='labeltext' and text()='1 Stop']//parent::span//parent::label[@for='filter_stop1']/span/span")
 	private WebElement oneStop;
-	
-	@FindBy(xpath="//div[@id='fli_filter__stops']//a[text()='Reset'][1]")
+
+	@FindBy(xpath = "//div[@id='fli_filter__stops']//a[text()='Reset'][1]")
 	private WebElement reset;
-	
-	@FindBy(xpath="//a[@class='active makeFlex hrtlCenter column']//span[@class='chNavIcon appendBottom2 chSprite chFlights active']")
-    private WebElement activeFlightLogo;
-	
-	@FindBy(xpath="//div[@class='splitVw-footer-left ']//div[4]/p[@class='actual-price']/span")
+
+	@FindBy(xpath = "//a[@class='active makeFlex hrtlCenter column']//span[@class='chNavIcon appendBottom2 chSprite chFlights active']")
+	private WebElement activeFlightLogo;
+
+	@FindBy(xpath = "//div[@class='splitVw-footer-left ']//div[4]/p[@class='actual-price']/span")
 	private WebElement actual_price_of_departure;
-	
-	@FindBy(xpath="//div[@class='splitVw-footer-right ']//div[4]/p[@class='actual-price']/span")
+
+	@FindBy(xpath = "//div[@class='splitVw-footer-right ']//div[4]/p[@class='actual-price']/span")
 	private WebElement actual_price_of_return;
-	
-	@FindBy(xpath="//div[@class='footer-fare']/p/span/span")
+
+	@FindBy(xpath = "//div[@class='footer-fare']/p/span/span")
 	private WebElement totalPrice;
-	
-	@FindBy(xpath="//div[@class='pull-left font14 LatoBold splitVw-sort-sctn']/p")
+
+	@FindBy(xpath = "//div[@class='pull-left font14 LatoBold splitVw-sort-sctn']/p")
 	private List<WebElement> dateAndPlace;
-	
-	public FlightsPage(){
+
+	public FlightsPage() {
 		PageFactory.initElements(driver, this);
 	}
-	
-	public void get_Place_and_Date(){
-		System.out.println("******Journy Details******");
+
+	//get city names and date of journey
+	public void get_Place_and_Date() {
+		System.out.println("******Journey Details******");
 		System.out.println("");
-		int i=1;
-		
-		for(WebElement element:dateAndPlace){
-			String placeDetails=element.getText();
+		int i = 1;
+
+		for (WebElement element : dateAndPlace) {
+			String placeDetails = element.getText();
 			System.out.println(placeDetails);
-			if(i==2){
+			if (i == 2) {
 				System.out.println("");
 			}
 			i++;
 		}
 		System.out.println("");
-		
+
 	}
-	//Reset all checkboxes
-	public void resetAllCheckBoxes(){
-          reset.click();
+
+	// Reset all checkboxes
+	public void resetAllCheckBoxes() {
+		reset.click();
 	}
-	
-	//click on checkbox of Non Stop
-	public void click_On_Non_Stop(){
+
+	// click on checkbox of Non Stop
+	public void click_On_Non_Stop() {
 		nonStop.click();
 	}
-	
-	//click on checkbox of One Stop
-	public void click_On_One_Stop(){
+
+	// click on checkbox of One Stop
+	public void click_On_One_Stop() {
 		oneStop.click();
 	}
-	
-	//Scrolling page
-	public void pageScrollDown() throws InterruptedException{
+
+	// Scrolling page
+	public void pageScrollDown() throws InterruptedException {
 		Thread.sleep(5000);
-		int scroll=2000;
-		for(int i=0;i<=10;i++){
-			String scrollStr=Integer.toString(scroll);
-			UtilClass.scrollPageDown(driver,scrollStr);
+		int scroll = 2000;
+		for (int i = 0; i <= 10; i++) {
+			String scrollStr = Integer.toString(scroll);
+			UtilClass.scrollPageDown(driver, scrollStr);
 			Thread.sleep(2000);
-			scroll+=2000;
+			scroll += 2000;
 		}
-		
-		
+
 		UtilClass.scrollPageDown(driver);
 		Thread.sleep(5000);
-		UtilClass.scrollPage(driver,activeFlightLogo);
+		UtilClass.scrollPage(driver, activeFlightLogo);
 		Thread.sleep(5000);
 
 	}
-	
-	//get total noOf departure Flights
-	public int get_noOf_departureFlights(){
-		
-		//wait.until(ExpectedConditions.visibilityOfAllElements(departureFlights));
-		int totalnoOfFlights=departureFlights.size();
-		
-		return totalnoOfFlights;
-	}
-	
-	//get total noOf return Flights
-	public int get_noOf_returnFlights(){
 
-		//wait.until(ExpectedConditions.visibilityOfAllElements(returnFlights));
-		int totalnoOfFlights=returnFlights.size();
-		
+	// get total noOf departure Flights
+	public int get_noOf_departureFlights() {
+
+		// wait.until(ExpectedConditions.visibilityOfAllElements(departureFlights));
+		int totalnoOfFlights = departureFlights.size();
+
 		return totalnoOfFlights;
 	}
-	
-	//return active flight_logo WebElement
-	public WebElement get_active_flight_logo(){
+
+	// get total noOf return Flights
+	public int get_noOf_returnFlights() {
+
+		// wait.until(ExpectedConditions.visibilityOfAllElements(returnFlights));
+		int totalnoOfFlights = returnFlights.size();
+
+		return totalnoOfFlights;
+	}
+
+	// return active flight_logo WebElement
+	public WebElement get_active_flight_logo() {
 		return activeFlightLogo;
 	}
-	
-	//return title of Flight Page
-	public String get_title_Of_FlightPage(){
+
+	// return title of Flight Page
+	public String get_title_Of_FlightPage() {
 		return driver.getTitle();
 	}
-	
-	//select departure flight
-	public void select_departure_Flight(int num){
-		String flightNum=Integer.toString(num);
-		String flight="//div[@class='splitVw-sctn pull-left']//div[2]//div[@class='fli-list splitVw-listing']["+flightNum+"]//label[@class='splitVw-radio clearfix cursor_pointer']/div[1]/span[1]/span";
-		
-		UtilClass.scrollPage(driver,driver.findElement(By.xpath(flight)));
-		WebElement radio=driver.findElement(By.xpath(flight));
-		if(!radio.isSelected()){
+
+	// select departure flight
+	public void select_departure_Flight(int num) {
+		String flightNum = Integer.toString(num);
+		String flight = "//div[@class='splitVw-sctn pull-left']//div[2]//div[@class='fli-list splitVw-listing']["
+				+ flightNum + "]//label[@class='splitVw-radio clearfix cursor_pointer']/div[1]/span[1]/span";
+
+		UtilClass.scrollPage(driver, driver.findElement(By.xpath(flight)));
+		WebElement radio = driver.findElement(By.xpath(flight));
+		if (!radio.isSelected()) {
 			UtilClass.clickOnElement(driver, radio);
 		}
 	}
-	
-	//select return flight
-	public void select_return_Flight(int num){
-		String flightNum=Integer.toString(num);
-		String flight="//div[@class='splitVw-sctn pull-right']//div[2]//div[@class='fli-list splitVw-listing']["+flightNum+"]//label[@class='splitVw-radio clearfix cursor_pointer']/div[1]/span[1]/span";
-		
-		UtilClass.scrollPage(driver,driver.findElement(By.xpath(flight)));
-		WebElement radio=driver.findElement(By.xpath(flight));
-		if(!radio.isSelected()){
+
+	// select return flight
+	public void select_return_Flight(int num) {
+		String flightNum = Integer.toString(num);
+		String flight = "//div[@class='splitVw-sctn pull-right']//div[2]//div[@class='fli-list splitVw-listing']["
+				+ flightNum + "]//label[@class='splitVw-radio clearfix cursor_pointer']/div[1]/span[1]/span";
+
+		UtilClass.scrollPage(driver, driver.findElement(By.xpath(flight)));
+		WebElement radio = driver.findElement(By.xpath(flight));
+		if (!radio.isSelected()) {
 			UtilClass.clickOnElement(driver, radio);
 		}
 	}
-	
-	//get price of DepartureFlight
-	public int get_Price_of_DepartureFlight(int num){
-		String flightNum=Integer.toString(num);
-		String price="//div[@class='splitVw-sctn pull-left']//div[2]//div[@class='fli-list splitVw-listing']["+flightNum+"]//label[@class='splitVw-radio clearfix cursor_pointer']/div[2]/div[3]/p/span[text()]";
-		
-		UtilClass.scrollPage(driver,driver.findElement(By.xpath(price)));
-		String priceOfFlight=driver.findElement(By.xpath(price)).getText().replaceAll("[^0-9]","");
-		int priceInt=Integer.parseInt(priceOfFlight);
-		
+
+	// get price of DepartureFlight
+	public int get_Price_of_DepartureFlight(int num) {
+		String flightNum = Integer.toString(num);
+		String price = "//div[@class='splitVw-sctn pull-left']//div[2]//div[@class='fli-list splitVw-listing']["
+				+ flightNum + "]//label[@class='splitVw-radio clearfix cursor_pointer']/div[2]/div[3]/p/span[text()]";
+
+		UtilClass.scrollPage(driver, driver.findElement(By.xpath(price)));
+		String priceOfFlight = driver.findElement(By.xpath(price)).getText().replaceAll("[^0-9]", "");
+		int priceInt = Integer.parseInt(priceOfFlight);
+
 		return priceInt;
 	}
-	
-	//get price of return flight
-	public int get_Price_of_ReturnFlight(int num){
-		String flightNum=Integer.toString(num);
-		String price="//div[@class='splitVw-sctn pull-right']//div[2]//div[@class='fli-list splitVw-listing']["+flightNum+"]//label[@class='splitVw-radio clearfix cursor_pointer']/div[2]/div[3]/p/span[text()]";
-		
-		
-		UtilClass.scrollPage(driver,driver.findElement(By.xpath(price)));
-		
-		
-		String priceOfFlight=driver.findElement(By.xpath(price)).getText().replaceAll("[^0-9]","");
-		int priceInt=Integer.parseInt(priceOfFlight);
+
+	// get price of return flight
+	public int get_Price_of_ReturnFlight(int num) {
+		String flightNum = Integer.toString(num);
+		String price = "//div[@class='splitVw-sctn pull-right']//div[2]//div[@class='fli-list splitVw-listing']["
+				+ flightNum + "]//label[@class='splitVw-radio clearfix cursor_pointer']/div[2]/div[3]/p/span[text()]";
+
+		UtilClass.scrollPage(driver, driver.findElement(By.xpath(price)));
+
+		String priceOfFlight = driver.findElement(By.xpath(price)).getText().replaceAll("[^0-9]", "");
+		int priceInt = Integer.parseInt(priceOfFlight);
 		return priceInt;
 	}
-	
-	//get actual price of departure flight
-	public int get_actual_departure_price(){
-		UtilClass.scrollPage(driver,actual_price_of_departure);
-		String price=actual_price_of_departure.getText().replaceAll("[^0-9]","");
-		
-		int priceInt=Integer.parseInt(price);
+
+	// get actual price of departure flight
+	public int get_actual_departure_price() {
+		UtilClass.scrollPage(driver, actual_price_of_departure);
+		String price = actual_price_of_departure.getText().replaceAll("[^0-9]", "");
+
+		int priceInt = Integer.parseInt(price);
 		return priceInt;
 	}
-	
-	//get actual price of return flight
-	public int get_actual_return_price(){
-		UtilClass.scrollPage(driver,actual_price_of_return);
-		String price=actual_price_of_return.getText().replaceAll("[^0-9]","");
-		int priceInt=Integer.parseInt(price);
+
+	// get actual price of return flight
+	public int get_actual_return_price() {
+		UtilClass.scrollPage(driver, actual_price_of_return);
+		String price = actual_price_of_return.getText().replaceAll("[^0-9]", "");
+		int priceInt = Integer.parseInt(price);
 		return priceInt;
 	}
-	
-	//get total price of tickets
-	public int get_total_price(){
-		UtilClass.scrollPage(driver,totalPrice);
-		String stringPrice=totalPrice.getText().replaceAll("[^0-9]","");
-		
-		int priceInt=Integer.parseInt(stringPrice);
-		
+
+	// get total price of tickets
+	public int get_total_price() {
+		UtilClass.scrollPage(driver, totalPrice);
+		String stringPrice = totalPrice.getText().replaceAll("[^0-9]", "");
+
+		int priceInt = Integer.parseInt(stringPrice);
+
 		return priceInt;
 	}
-	
-	public int get_top_flights(int maxflightsnum){
-		
-		int maxFlightsValue=0;
-		
-		if(maxflightsnum<10){
-			maxFlightsValue=maxflightsnum;
-		}else if(maxflightsnum>=10){
-			maxFlightsValue=10;
+
+	// get top ten flights displayed on the flight page
+	public int get_top_ten_flights(int maxflightsnumber) {
+
+		int maxFlightsValue = 0;
+
+		if (maxflightsnumber < 10) {
+
+			maxFlightsValue = maxflightsnumber;
+
+		} else if (maxflightsnumber >= 10) {
+
+			maxFlightsValue = 10;
 		}
-		
+
 		return maxFlightsValue;
-		
+
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
--- src/test/java/com/PageTestClasses/FlightPageTest.java
+++ src/test/java/com/PageTestClasses/FlightPageTest.java
@@ -22,20 +22,20 @@ public class FlightPageTest extends BaseClass {
 
 	public HomePage homePage;
 	public FlightsPage flightsPage;
-	public int totalDFlights=0;
+	public int totalDFlights = 0;
 
-	public int totalRFlights=0;
+	public int totalRFlights = 0;
 
 	public FlightPageTest() {
 		super();
 	}
 
-	//Initializing the Application 
+	// Initializing the Application
 	@BeforeClass
 	public void openBrowser() {
 		initialization();
 		homePage = new HomePage();
-		
+
 		Assert.assertEquals(homePage.getHomePageTitle(), UtilClass.homepageTitle);
 		homePage.clickOnFlights();
 		homePage.clickOnRoundtrip();
@@ -51,32 +51,32 @@ public class FlightPageTest extends BaseClass {
 		flightsPage = homePage.clickOnSearch();
 	}
 
-	//validating title of the flight page
-	//validating active flight logo is present or not
-	@Test(priority=1)
+	// validating title of the flight page
+	// validating active flight logo is present or not
+	@Test(priority = 1)
 	public void validateFlightPageTest() {
 
 		boolean activeFlightLogo = flightsPage.get_active_flight_logo().isDisplayed();
 		Assert.assertEquals(activeFlightLogo, true);
 
 		Assert.assertEquals(flightsPage.get_title_Of_FlightPage(), UtilClass.flightPageTitle);
-		System.out.println("Title of the flight page-->"+flightsPage.get_title_Of_FlightPage());
+		System.out.println("Title of the flight page-->" + flightsPage.get_title_Of_FlightPage());
 		System.out.println("");
-		
+
 		try {
 			flightsPage.get_Place_and_Date();
 		} catch (Exception e) {
-			
-			System.out.println("****Error while geting Journy details****");
+
+			System.out.println("****Error while geting Journey details****");
 		}
 	}
 
-	//print number of all type of Flights present on the page
-	@Test(priority=2)
+	// print number of all type of Flights present on the page
+	@Test(priority = 2)
 	public void display_noOf_FlightTest() throws InterruptedException {
-        flightsPage.pageScrollDown();
+		flightsPage.pageScrollDown();
 		int totaldepartureFlights = flightsPage.get_noOf_departureFlights();
-            
+
 		System.out.println("Total NoOf_departureFlights = " + totaldepartureFlights);
 		System.out.println("");
 
@@ -85,19 +85,19 @@ public class FlightPageTest extends BaseClass {
 		System.out.println("Total NoOf_ReturnFlights = " + totalReturnFlights);
 
 		System.out.println("");
-		
-		totalDFlights=totaldepartureFlights;
-		totalRFlights=totalReturnFlights;
+
+		totalDFlights = totaldepartureFlights;
+		totalRFlights = totalReturnFlights;
 	}
 
-	//print number of Non-Stop Flights present on the page
-	@Test(priority=3)
+	// print number of Non-Stop Flights present on the page
+	@Test(priority = 3)
 	public void display_noOf_Non_Stop_FlightTest() throws InterruptedException {
 		flightsPage.resetAllCheckBoxes();
 		flightsPage.click_On_Non_Stop();
-        
+
 		flightsPage.pageScrollDown();
-		
+
 		int totaldepartureFlights = flightsPage.get_noOf_departureFlights();
 
 		System.out.println("================No Of Non Stop Flights===================");
@@ -113,12 +113,12 @@ public class FlightPageTest extends BaseClass {
 		flightsPage.resetAllCheckBoxes();
 	}
 
-	//print number of Single-Stop Flights present on the page
-	@Test(priority=4)
+	// print number of Single-Stop Flights present on the page
+	@Test(priority = 4)
 	public void display_noOf_One_Stop_FlightTest() throws InterruptedException {
 		flightsPage.resetAllCheckBoxes();
 		flightsPage.click_On_One_Stop();
-		
+
 		flightsPage.pageScrollDown();
 
 		int totaldepartureFlights = flightsPage.get_noOf_departureFlights();
@@ -136,72 +136,81 @@ public class FlightPageTest extends BaseClass {
 		flightsPage.resetAllCheckBoxes();
 	}
 
-	//click on radio buttons of the flights
-	//getting the price of the Flights
-	//compare selected flight price and actual flight price displayed on bottom of the page
-	//validate total price of the Flights
-	@Test(priority=5)
+	// click on radio buttons of the flights
+	// getting the price of the Flights
+	// compare selected flight price and actual flight price displayed on bottom
+	// of the page
+	// validate total price of the Flights
+	@Test(priority = 5)
 	public void selectFlightTest() throws InterruptedException {
-		
-		//get max departure flights numbers to select
-		int maxDepFlightValue=flightsPage.get_top_flights(totalDFlights);
-		//get max flights number to select
-		int maxRetFlightValue=flightsPage.get_top_flights(totalRFlights);
-		
-        //validating top ten flights price ten times by selecting random flights
-		for (int i = 1; i <=10; i++) {
 
-			//get random departure flights numbers between 1 and maxDepFlightValue
-			int depFlightNum=UtilClass.get_Random_Number(maxDepFlightValue);
-			//get random return flights numbers between 1 and maxRetFlightValue
-			int retFlightNum=UtilClass.get_Random_Number(maxRetFlightValue);
+		// get max departure flights numbers to select
+		int maxDepFlightValue = flightsPage.get_top_ten_flights(totalDFlights);
+		// get max flights number to select
+		int maxRetFlightValue = flightsPage.get_top_ten_flights(totalRFlights);
+
+		// validating top ten flights price ten times by selecting random
+		// flights
+		for (int i = 1; i <= 10; i++) {
+
+			// get random departure flights numbers between 1 and
+			// maxDepFlightValue
+			int depFlightNum = UtilClass.get_Random_Number(maxDepFlightValue);
 			
-			//click on departure flight radio button
+			// get random return flights numbers between 1 and maxRetFlightValue
+			int retFlightNum = UtilClass.get_Random_Number(maxRetFlightValue);
+			
+			//if depFlightNum and retFlightNum is equal
+			while (depFlightNum == retFlightNum) {
+				retFlightNum = UtilClass.get_Random_Number(maxRetFlightValue);
+			}
+
+			// click on departure flight radio button
 			flightsPage.select_departure_Flight(depFlightNum);
-			//click on return flight radio button
+			// click on return flight radio button
 			flightsPage.select_return_Flight(retFlightNum);
 
-			//get price of selected departure flight
+			// get price of selected departure flight
 			int selectedDeparturePrice = flightsPage.get_Price_of_DepartureFlight(depFlightNum);
-			//get price of selected return flight
+			// get price of selected return flight
 			int selectedReturnPrice = flightsPage.get_Price_of_ReturnFlight(retFlightNum);
 
-			//get actual price of departure flight displayed on bottom of the page
+			// get actual price of departure flight displayed on bottom of the
+			// page
 			int actualDeparturePrice = flightsPage.get_actual_departure_price();
-			//get actual price of return flight displayed on bottom of the page
+			// get actual price of return flight displayed on bottom of the page
 			int actualReturnPrice = flightsPage.get_actual_return_price();
-			
-			//print flights price on console
+
+			// print flights price on console
 			System.out.println("                          selected    Actual");
-			System.out.println(depFlightNum+". DepartureFlightPrice->Rs."+selectedDeparturePrice + "    Rs." + actualDeparturePrice);
-			System.out.println(retFlightNum+". ReturnFlightPriceo  ->Rs."+selectedReturnPrice + "     Rs." + actualReturnPrice);
-			
-			//get total price flights
+			System.out.println(depFlightNum + ". DepartureFlightPrice->Rs." + selectedDeparturePrice + "    Rs."
+					+ actualDeparturePrice);
+			System.out.println(retFlightNum + ". ReturnFlightPriceo  ->Rs." + selectedReturnPrice + "     Rs."
+					+ actualReturnPrice);
+
+			// get total price flights
 			int totalSelected = selectedDeparturePrice + selectedReturnPrice;
-			
-			//print total price on console
+
+			// print total price on console
 			System.out.println("Total Selected Flights Price = Rs." + totalSelected);
 			System.out.println("Tatal Actual Flights Price   = Rs." + flightsPage.get_total_price());
 			System.out.println("============================================");
 
-			//check flights price equal or not
+			// check flights price equal or not
 			Assert.assertEquals(selectedDeparturePrice, actualDeparturePrice);
 			Assert.assertEquals(selectedReturnPrice, actualReturnPrice);
 
-			//check flights total price equal or not
+			// check flights total price equal or not
 			int totalprice = selectedDeparturePrice + selectedReturnPrice;
 			Assert.assertEquals(totalprice, flightsPage.get_total_price());
-			
-		
-			 
 
 		}
 
 	}
-	
-	//close Browser
+
+	// close Browser
 	@AfterClass
-	public void closeBrowser(){
+	public void closeBrowser() {
 		killBrowser();
 	}
 
